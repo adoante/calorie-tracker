be9898a8fe202c8dd1a19de04f0df636b8adc4c8
Adolfo Gante <adolfogante@gmail.com>
2025-11-17 12:58:11 -0800

    feat: Initial Dashboard page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import RemainingCalories from "@/components/RemainingCalories";
+
+export default function Dashboard() {
+    return (
+        <main>
+            <h1>Dashboard</h1>
+
+            <RemainingCalories />
+
+        </main>
+    )
+}
