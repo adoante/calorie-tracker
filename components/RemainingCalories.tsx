ae7bc063d77d0d9614d6dd4b7b88176ab0fe7528
Adolfo Gante <adolfogante@gmail.com>
2025-11-17 12:57:39 -0800

    feat: Calories Remaining Card (no data fetching) component

---
+++ components/RemainingCalories.tsx
@@ -0,0 +1,108 @@
+"use client"
+
+import {
+    Label,
+    PolarGrid,
+    PolarRadiusAxis,
+    RadialBar,
+    RadialBarChart,
+} from "recharts"
+
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+
+import { ChartConfig, ChartContainer } from "@/components/ui/chart"
+
+const chartData = [
+    { daily: 2500, current: 500, remaining: 1000, fill: "var(--color-default)" },
+]
+
+const chartConfig = {
+    default: {
+        color: "var(--chart-2)",
+    },
+} satisfies ChartConfig
+
+const today = new Date()
+const dateOptions: Intl.DateTimeFormatOptions = {
+    day: "numeric",
+    month: "long",
+    year: "numeric"
+}
+const todayFormatted = today.toLocaleString("en-UK", dateOptions)
+
+export default function RemainingCalories() {
+    return (
+        <Card className="flex flex-col">
+            <CardHeader className="items-center pb-0">
+                <CardTitle>Calories</CardTitle>
+                <CardDescription>{todayFormatted}</CardDescription>
+            </CardHeader>
+            <CardContent className="flex-1 pb-0">
+                <ChartContainer
+                    config={chartConfig}
+                    className="mx-auto aspect-square max-h-[250px]"
+                >
+                    <RadialBarChart
+                        data={chartData}
+                        startAngle={0}
+                        endAngle={(chartData[0].current / chartData[0].daily) * 360}
+                        innerRadius={80}
+                        outerRadius={110}
+                    >
+                        <PolarGrid
+                            gridType="circle"
+                            radialLines={false}
+                            stroke="none"
+                            className="first:fill-muted last:fill-background"
+                            polarRadius={[86, 74]}
+                        />
+                        <RadialBar dataKey="current" background cornerRadius={10} />
+                        <PolarRadiusAxis tick={false} tickLine={false} axisLine={false}>
+                            <Label
+                                content={({ viewBox }) => {
+                                    if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                                        return (
+                                            <text
+                                                x={viewBox.cx}
+                                                y={viewBox.cy}
+                                                textAnchor="middle"
+                                                dominantBaseline="middle"
+                                            >
+                                                <tspan
+                                                    x={viewBox.cx}
+                                                    y={viewBox.cy}
+                                                    className="fill-foreground text-4xl font-bold"
+                                                >
+                                                    {chartData[0].current.toLocaleString()}
+                                                </tspan>
+                                                <tspan
+                                                    x={viewBox.cx}
+                                                    y={(viewBox.cy || 0) + 24}
+                                                    className="fill-muted-foreground"
+                                                >
+                                                    Calories
+                                                </tspan>
+                                            </text>
+                                        )
+                                    }
+                                }}
+                            />
+                        </PolarRadiusAxis>
+                    </RadialBarChart>
+                </ChartContainer>
+            </CardContent>
+            <CardFooter className="flex-col gap-2 text-sm">
+                <div className="flex items-center gap-2 leading-none font-medium">
+                    Remaining calories are {chartData[0].remaining}
+                </div>
+            </CardFooter>
+        </Card>
+    )
+}
