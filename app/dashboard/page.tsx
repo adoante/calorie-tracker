baa3ccb72b0f6bbcee2e44c6db937bd4ff44766c
Adolfo Gante <adolfogante@gmail.com>
2025-11-17 17:48:32 -0800

    feat: basic dashbord

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,23 @@
 import RemainingCalories from "@/components/RemainingCalories";
+import StreakCounter from "@/components/StreakCounter";
+import { WeeklyCaloriesBarChart } from "@/components/CalorieSummaryCharts";
 
 export default function Dashboard() {
     return (
-        <main>
-            <h1>Dashboard</h1>
+        <main className="space-y-5">
+            <h1 className="text-4xl text-center font-bold">
+                Dashboard
+            </h1>
+
+            <StreakCounter
+                title="Streak Counter"
+                desc="Displays streak count"
+                count={100}
+            />
 
             <RemainingCalories />
+
+            <WeeklyCaloriesBarChart />
 
         </main>
     )
